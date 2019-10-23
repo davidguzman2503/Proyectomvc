5083c51db5fcfb798a4c06107f914b1095ce2824
skn_2 <skn_2@10.28.201.180>
2019-10-22 21:30:57 -0500

    Primera version del prpyecto MVC DB

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("remotemysql.com", "7tK58L6A24", "4iULrSz7Xe", "7tK58L6A24");
+        $conexion=new mysqli("remotemysql.com", "rCLbWgFn3D", "rCLbWgFn3D", "UYxEIzY2TO");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
